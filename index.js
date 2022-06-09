4faf8579a0cc2ad589f7a23457db5a0e0346f443
syphrose <syphrose.wabulwenyi@student.moringaschool.com>
2022-06-10 02:37:17 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+//Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1* num2;
+console.log(multiply);
 
+const num3 = 25;
+const num4 = 7;
+const mod = num3% num4;
+console.log(mod);
+
+const max = Math.max(17,12,6,20,14);
+const random = Math.floor(Math.random()*10)+3;
